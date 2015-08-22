e87d4afbaed32195bb974797bdefd3689474b35f
Hồ Ngọc Triển <hongoctrien@vinades.vn>
2015-08-22 11:26:03 +0700

    Thay đổi đường dẫn các thư mục js, images, editors ở thư mục gốc

--- modules/dispatch/blocks/global.block_category.php
+++ modules/dispatch/blocks/global.block_category.php
@@ -20,7 +20,7 @@ if ( ! nv_function_exists( 'nv_dispathch_category' ) )
         for ( $i = 0; $i < 100; $i ++ )
         {
             $sel = ( $data_block['title_length'] == $i ) ? ' selected' : '';
-            
+
             $html .= "<option value=\"" . $i . "\" " . $sel . ">" . $i . "</option>\n";
         }
         $html .= "</select></td>\n";
@@ -40,10 +40,13 @@ if ( ! nv_function_exists( 'nv_dispathch_category' ) )
     function nv_dispathch_category ( $block_config )
     {
         global $module_array_cat, $module_info, $lang_module;
+
         $xtpl = new XTemplate( "block_category.tpl", NV_ROOTDIR . "/themes/" . $module_info['template'] . "/modules/dispatch" );
-        $xtpl->assign( 'LANG', $lang_module );  
+        $xtpl->assign( 'LANG', $lang_module );
+        $xtpl->assign( 'NV_ASSETS_DIR', NV_ASSETS_DIR );
+
         $arr = array();
-               
+
         if ( ! empty( $module_array_cat ) )
         {
             $title_length = $block_config['title_length'];
@@ -51,20 +54,20 @@ if ( ! nv_function_exists( 'nv_dispathch_category' ) )
             $xtpl->assign( 'BLOCK_ID', $block_config['bid'] );
             $xtpl->assign( 'TEMPLATE', $module_info['template'] );
             $html = "";
-           
+
             foreach ( $module_array_cat as $cat )
-            {            	
-            	
+            {
+
                 if ( $cat['parentid'] == 0 )
                 {
-                	
+
                     $html .= "<li>\n";
                     $html .= "<a title=\"" . $cat['title'] . "\" href=\"" . $cat['link'] . "\">" . nv_clean60( $cat['title'], $title_length ) . "</a>\n";
                     if ( ! empty( $cat['subcatid'] ) ) $html .= nv_dispathch_sub_category( $cat['subcatid'], $title_length );
                     $html .= "</li>\n";
-                }                
+                }
             }
-            
+
             $xtpl->assign( 'HTML_CONTENT', $html );
             $xtpl->parse( 'main' );
             return $xtpl->text( 'main' );
@@ -84,7 +87,7 @@ if ( ! nv_function_exists( 'nv_dispathch_category' ) )
             $html = "<ul>\n";
             foreach ( $list as $catid )
             {
-            	           	
+
             	$html .= "<li>\n";
                 $html .= "<a title=\"" . $module_array_cat[$catid]['title'] . "\" href=\"" . $module_array_cat[$catid]['link'] . "\">" . nv_clean60( $module_array_cat[$catid]['title'], $title_length ) . "</a>\n";
                 if ( ! empty( $module_array_cat[$catid]['subcatid'] ) ) $html .= nv_dispathch_sub_category( $module_array_cat[$catid]['subcatid'], $title_length );
@@ -101,13 +104,13 @@ if ( defined( 'NV_SYSTEM' ) )
     global $site_mods, $module_name, $global_array_cat, $module_array_cat;
     $module = $block_config['module'];
     if ( isset( $site_mods[$module] ) )
-    {       
+    {
             $module_array_cat = array();
             $sql = "SELECT id, parentid, title, alias FROM `" . NV_PREFIXLANG . "_" . $site_mods[$module]['module_data'] . "_type` ORDER BY `parentid`,`weight` ASC";
             $list = nv_db_cache( $sql, 'id', $module );
             foreach ( $list as $l )
             {
-            	$module_array_cat[$l['id']] = $l;            	
+            	$module_array_cat[$l['id']] = $l;
             	$module_array_cat[$l['id']]['subcatid'] = '';
                 $module_array_cat[$l['id']]['link'] = NV_BASE_SITEURL . "index.php?" . NV_LANG_VARIABLE . "=" . NV_LANG_DATA . "&amp;" . NV_NAME_VARIABLE . "=" . $module . "&amp;op=main&type=" . $l['id'];
 	            if ( $module_array_cat[$l['id']]['parentid'] != 0 )
@@ -116,14 +119,14 @@ if ( defined( 'NV_SYSTEM' ) )
 	            	{
 	                $module_array_cat[$module_array_cat[$l['id']]['parentid']]['subcatid'] = $module_array_cat[$l['id']]['id'];
 	            	}
-	            	else 
+	            	else
 	            	{
 	            		$module_array_cat[$module_array_cat[$l['id']]['parentid']]['subcatid'] = $module_array_cat[$module_array_cat[$l['id']]['parentid']]['subcatid'].",".$module_array_cat[$l['id']]['id'];
 	            	}
 	            }
-	            
+
             }
-        
+
         $content = nv_dispathch_category( $block_config );
     }
 }
