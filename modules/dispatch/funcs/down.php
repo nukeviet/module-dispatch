cfc8987d30f3ddc33352bd148de3d575df0bb5a2
Phan Tan Dung <phantandung92@gmail.com>
2016-09-28 16:37:23 +0700

    Security data

--- modules/dispatch/funcs/down.php
+++ modules/dispatch/funcs/down.php
@@ -8,72 +8,64 @@
  * @Createdate 3-6-2010 0:30
  */
 
-if ( ! defined( 'NV_IS_MOD_DOWNLOAD' ) ) die( 'Stop!!!' );
+if (!defined('NV_IS_MOD_DOWNLOAD'))
+    die('Stop!!!');
 
-if ( ! $nv_Request->isset_request( 'session_files', 'session' ) )
-{
-    die( 'Wrong URL' );
+if (!$nv_Request->isset_request('session_files', 'session')) {
+    die('Wrong URL');
 }
 
-$session_files = $nv_Request->get_string( 'session_files', 'session', '' );
+$session_files = $nv_Request->get_string('session_files', 'session', '');
 
-if ( empty( $session_files ) )
-{
-    die( 'Wrong URL' );
+if (empty($session_files)) {
+    die('Wrong URL');
 }
 
-$session_files = unserialize( $session_files );
+$session_files = unserialize($session_files);
 
-if ( $nv_Request->isset_request( 'code', 'get' ) )
-{
-    $code = $nv_Request->get_string( 'code', 'get', '' );
+if ($nv_Request->isset_request('code', 'get')) {
+    $code = $nv_Request->get_string('code', 'get', '');
 
-    if ( empty( $code ) or ! preg_match( "/^([a-z0-9]{32})$/i", $code ) or ! isset( $session_files['linkdirect'][$code] ) or ! nv_check_url( $session_files['linkdirect'][$code]['link'] ) )
-    {
-        die( 'Wrong URL' );
+    if (empty($code) or !preg_match("/^([a-z0-9]{32})$/i", $code) or !isset($session_files['linkdirect'][$code]) or !nv_check_url($session_files['linkdirect'][$code]['link'])) {
+        die('Wrong URL');
     }
 
-    $sql = "UPDATE " . NV_PREFIXLANG . "_" . $module_data . " SET download_hits=download_hits+1 WHERE id=" . intval( $session_files['linkdirect'][$code]['id'] );
-    $db->query( $sql );
+    $sql = "UPDATE " . NV_PREFIXLANG . "_" . $module_data . " SET download_hits=download_hits+1 WHERE id=" . intval($session_files['linkdirect'][$code]['id']);
+    $db->query($sql);
 
-    $content = "<br /><img border=\"0\" src=\"" . NV_BASE_SITEURL  . NV_ASSETS_DIR . "images/load_bar.gif\"><br /><br />\n";
-    $content .= sprintf( $lang_module['download_wait2'], $session_files['linkdirect'][$code]['link'] );
+    $content = "<br /><img border=\"0\" src=\"" . NV_BASE_SITEURL . NV_ASSETS_DIR . "images/load_bar.gif\"><br /><br />\n";
+    $content .= sprintf($lang_module['download_wait2'], $session_files['linkdirect'][$code]['link']);
     $content .= "<meta http-equiv=\"refresh\" content=\"5;url=" . $session_files['linkdirect'][$code]['link'] . "\" />";
 
-    nv_info_die( $lang_module['download_detail'], $lang_module['download_wait'], $content );
+    nv_info_die($lang_module['download_detail'], $lang_module['download_wait'], $content);
 
     die();
 }
 
-if ( ! $nv_Request->isset_request( 'file', 'get' ) )
-{
-    die( 'Wrong URL' );
+if (!$nv_Request->isset_request('file', 'get')) {
+    die('Wrong URL');
 }
 
-$file = $nv_Request->get_string( 'file', 'get', '' );
+$file = $nv_Request->get_string('file', 'get', '');
 
-if ( empty( $file ) )
-{
-    die( 'Wrong URL' );
+if (empty($file)) {
+    die('Wrong URL');
 }
 
-if ( ! isset( $session_files['fileupload'][$file] ) )
-{
-    die( 'Wrong URL' );
+if (!isset($session_files['fileupload'][$file])) {
+    die('Wrong URL');
 }
 
-if ( ! file_exists( $session_files['fileupload'][$file]['src'] ) )
-{
-    die( 'Wrong URL' );
+if (!file_exists($session_files['fileupload'][$file]['src'])) {
+    die('Wrong URL');
 }
 
-if ( ! isset( $session_files['fileupload'][$file]['id'] ) )
-{
-    die( 'Wrong URL' );
+if (!isset($session_files['fileupload'][$file]['id'])) {
+    die('Wrong URL');
 }
 
-$sql = "UPDATE " . NV_PREFIXLANG . "_" . $module_data . " SET download_hits=download_hits+1 WHERE id=" . intval( $session_files['fileupload'][$file]['id'] );
-$db->query( $sql );
+$sql = "UPDATE " . NV_PREFIXLANG . "_" . $module_data . " SET download_hits=download_hits+1 WHERE id=" . intval($session_files['fileupload'][$file]['id']);
+$db->query($sql);
 
 $upload_dir = "files";
 $is_zip = false;
@@ -81,21 +73,16 @@ $is_resume = false;
 $max_speed = 0;
 
 $sql = "SELECT config_name, config_value FROM " . NV_PREFIXLANG . "_" . $module_data . "_config WHERE config_name='upload_dir' OR config_name='is_zip' OR config_name='is_resume' OR config_name='max_speed'";
-$result = $db->query( $sql );
-while ( $row = $result->fetch() )
-{
-    if ( $row['config_name'] == 'upload_dir' )
-    {
+$result = $db->query($sql);
+while ($row = $result->fetch()) {
+    if ($row['config_name'] == 'upload_dir') {
         $upload_dir = $row['config_value'];
-    } elseif ( $row['config_name'] == 'is_zip' )
-    {
-        $is_zip = ( bool )$row['config_value'];
-    } elseif ( $row['config_name'] == 'is_resume' )
-    {
-        $is_resume = ( bool )$row['config_value'];
-    } elseif ( $row['config_name'] == 'max_speed' )
-    {
-        $max_speed = ( int )$row['config_value'];
+    } elseif ($row['config_name'] == 'is_zip') {
+        $is_zip = (bool)$row['config_value'];
+    } elseif ($row['config_name'] == 'is_resume') {
+        $is_resume = (bool)$row['config_value'];
+    } elseif ($row['config_name'] == 'max_speed') {
+        $max_speed = (int)$row['config_value'];
     }
 }
 
@@ -103,43 +90,35 @@ $file_src = $session_files['fileupload'][$file]['src'];
 $file_basename = $file;
 $directory = NV_UPLOADS_REAL_DIR;
 
-if ( $is_zip )
-{
+if ($is_zip) {
     $upload_dir = NV_UPLOADS_REAL_DIR . '/' . $module_upload . '/' . $upload_dir;
-    $subfile = nv_pathinfo_filename( $file );
+    $subfile = nv_pathinfo_filename($file);
     $tem_file = NV_ROOTDIR . '/' . NV_TEMP_DIR . '/' . NV_TEMPNAM_PREFIX . $subfile;
 
-    $file_exists = file_exists( $tem_file );
+    $file_exists = file_exists($tem_file);
 
-    if ( $file_exists and filemtime( $tem_file ) > NV_CURRENTTIME - 600 )
-    {
+    if ($file_exists and filemtime($tem_file) > NV_CURRENTTIME - 600) {
         $file_src = $tem_file;
         $file_basename = $subfile . '.zip';
         $directory = NV_ROOTDIR . '/' . NV_TEMP_DIR;
-    }
-    else
-    {
-        if ( $file_exists )
-        {
-            @nv_deletefile( $tem_file );
+    } else {
+        if ($file_exists) {
+            @nv_deletefile($tem_file);
         }
 
-        $zip = new PclZip( $tem_file );
+        $zip = new PclZip($tem_file);
 
-        $zip->add( $file_src, PCLZIP_OPT_REMOVE_PATH, $upload_dir );
+        $zip->add($file_src, PCLZIP_OPT_REMOVE_PATH, $upload_dir);
 
-        if ( isset( $global_config['site_logo'] ) and ! empty( $global_config['site_logo'] ) and file_exists( NV_ROOTDIR . '/' . $global_config['site_logo'] ) )
-        {
-            $zip->add( NV_ROOTDIR . '/' . $global_config['site_logo'], PCLZIP_OPT_REMOVE_PATH, NV_ROOTDIR . '/images' );
+        if (isset($global_config['site_logo']) and !empty($global_config['site_logo']) and file_exists(NV_ROOTDIR . '/' . $global_config['site_logo'])) {
+            $zip->add(NV_ROOTDIR . '/' . $global_config['site_logo'], PCLZIP_OPT_REMOVE_PATH, NV_ROOTDIR . '/images');
         }
 
-        if ( file_exists( NV_ROOTDIR . '/' . NV_DATADIR . '/README.txt' ) )
-        {
-            $zip->add( NV_ROOTDIR . '/' . NV_DATADIR . '/README.txt', PCLZIP_OPT_REMOVE_PATH, NV_ROOTDIR . '/' . NV_DATADIR );
+        if (file_exists(NV_ROOTDIR . '/' . NV_DATADIR . '/README.txt')) {
+            $zip->add(NV_ROOTDIR . '/' . NV_DATADIR . '/README.txt', PCLZIP_OPT_REMOVE_PATH, NV_ROOTDIR . '/' . NV_DATADIR);
         }
 
-        if ( file_exists( $tem_file ) )
-        {
+        if (file_exists($tem_file)) {
             $file_src = $tem_file;
             $file_basename = $subfile . '.zip';
             $directory = NV_ROOTDIR . '/' . NV_TEMP_DIR;
@@ -147,11 +126,10 @@ if ( $is_zip )
     }
 }
 
-$download = new NukeViet\Files\Download( $file_src, $directory, $file_basename, $is_resume, $max_speed );
-if ( $is_zip )
-{
-    $mtime = ( $mtime = filemtime( $session_files['fileupload'][$file]['src'] ) ) > 0 ? $mtime : time();
-    $download->set_property( 'mtime', $mtime );
+$download = new NukeViet\Files\Download($file_src, $directory, $file_basename, $is_resume, $max_speed);
+if ($is_zip) {
+    $mtime = ($mtime = filemtime($session_files['fileupload'][$file]['src'])) > 0 ? $mtime : time();
+    $download->set_property('mtime', $mtime);
 }
 $download->download_file();
 exit();
--- modules/dispatch/funcs/main.php
+++ modules/dispatch/funcs/main.php
@@ -8,8 +8,8 @@
  * @Createdate Tue, 19 Jul 2011 09:07:26 GMT
  */
 
-if( !defined( 'NV_IS_MOD_CONGVAN' ) )
-	die( 'Stop!!!' );
+if (!defined('NV_IS_MOD_CONGVAN'))
+    die('Stop!!!');
 
 $page_title = $module_info['custom_title'];
 $key_words = $module_info['keywords'];
@@ -17,202 +17,172 @@ $se = $from = $to = $from_signer = 0;
 $type = '';
 $code = $content = '';
 
-$array = array( );
+$array = array();
 $error = '';
 $sql = "FROM " . NV_PREFIXLANG . "_" . $module_data . "_document WHERE id!=0";
 $base_url = NV_BASE_SITEURL . "index.php?" . NV_NAME_VARIABLE . "=" . $module_name;
 
-$listcats = nv_listcats( 0 );
-$listdes = nv_listdes( 0 );
-$listtypes = nv_listtypes( $type );
+$listcats = nv_listcats(0);
+$listdes = nv_listdes(0);
+$listtypes = nv_listtypes($type);
 $page_title = $lang_module['table'];
 
-if( $nv_Request->isset_request( "se", "get" ) )
-{
-	$page_title = $lang_module['list_se'];
-	$se = $nv_Request->get_int( 'se', 'get', 0 );
+if ($nv_Request->isset_request("se", "get")) {
+    $page_title = $lang_module['list_se'];
+    $se = $nv_Request->get_int('se', 'get', 0);
 }
-if( $nv_Request->isset_request( "type", "get" ) )
-{
-	$type = $nv_Request->get_int( 'type', 'get', 0 );
-	if( $type != 0 )
-	{
-		$page_title = sprintf( $lang_module['cv_list_by_type'], $listtypes[$type]['title'] );
-		$a_t = array( );
-		$query = "SELECT id FROM " . NV_PREFIXLANG . "_" . $module_data . "_type WHERE id=" . $type . " OR parentid= " . $type;
-		$re = $db->query( $query );
-		while( $row = $re->fetch() )
-		{
-			$a_t[] = $row['id'];
-		}
-		$query = "SELECT id FROM " . NV_PREFIXLANG . "_" . $module_data . "_type WHERE id IN (" . implode( ',', $a_t ) . ")";
-		$re = $db->query( $query );
-		if( $row = $re->fetch() )
-		{
-			$a_t[] = $row['id'];
-		}
+if ($nv_Request->isset_request("type", "get")) {
+    $type = $nv_Request->get_int('type', 'get', 0);
+    if ($type != 0) {
+        $page_title = sprintf($lang_module['cv_list_by_type'], $listtypes[$type]['title']);
+        $a_t = array();
+        $query = "SELECT id FROM " . NV_PREFIXLANG . "_" . $module_data . "_type WHERE id=" . $type . " OR parentid= " . $type;
+        $re = $db->query($query);
+        while ($row = $re->fetch()) {
+            $a_t[] = $row['id'];
+        }
+        $query = "SELECT id FROM " . NV_PREFIXLANG . "_" . $module_data . "_type WHERE id IN (" . implode(',', $a_t) . ")";
+        $re = $db->query($query);
+        if ($row = $re->fetch()) {
+            $a_t[] = $row['id'];
+        }
 
-		$sql .= " AND type IN (" . implode( ',', $a_t ) . ")";
+        $sql .= " AND type IN (" . implode(',', $a_t) . ")";
 
-		$base_url .= "&amp;type=" . $type;
-	}
+        $base_url .= "&amp;type=" . $type;
+    }
 }
 
-if( $nv_Request->isset_request( "from", "get" ) )
-{
+if ($nv_Request->isset_request("from", "get")) {
 
-	$from = $nv_Request->get_title( 'from', 'get,post', '' );
+    $from = $nv_Request->get_title('from', 'get,post', '');
 
-	unset( $m );
-	if( preg_match( "/^([0-9]{1,2})\.([0-9]{1,2})\.([0-9]{4})$/", $from, $m ) )
-	{
-		$from = mktime( 0, 0, 0, $m[2], $m[1], $m[3] );
-	}
-	else
-	{
-		$from = 0;
-	}
+    unset($m);
+    if (preg_match("/^([0-9]{1,2})\.([0-9]{1,2})\.([0-9]{4})$/", $from, $m)) {
+        $from = mktime(0, 0, 0, $m[2], $m[1], $m[3]);
+    } else {
+        $from = 0;
+    }
 
-	if( $from != 0 )
-	{
-		//die($year.'');
+    if ($from != 0) {
+        //die($year.'');
 
-		$sql .= " AND from_time >= " . $from;
-		$base_url .= "&amp;from =" . $from;
-	}
+        $sql .= " AND from_time >= " . $from;
+        $base_url .= "&amp;from =" . $from;
+    }
 }
-if( $nv_Request->isset_request( "to", "get" ) )
-{
-	$to = $nv_Request->get_title( 'to', 'get,post', '' );
+if ($nv_Request->isset_request("to", "get")) {
+    $to = $nv_Request->get_title('to', 'get,post', '');
 
-	unset( $m );
-	if( preg_match( "/^([0-9]{1,2})\.([0-9]{1,2})\.([0-9]{4})$/", $to, $m ) )
-	{
-		$to = mktime( 0, 0, 0, $m[2], $m[1], $m[3] );
-	}
-	else
-	{
-		$to = 0;
-	}
-	if( $to != 0 )
-	{
-		//die($year.'');
+    unset($m);
+    if (preg_match("/^([0-9]{1,2})\.([0-9]{1,2})\.([0-9]{4})$/", $to, $m)) {
+        $to = mktime(0, 0, 0, $m[2], $m[1], $m[3]);
+    } else {
+        $to = 0;
+    }
+    if ($to != 0) {
+        //die($year.'');
 
-		$sql .= " AND from_time <= " . $to;
-		$base_url .= "&amp;to=" . $to;
-	}
+        $sql .= " AND from_time <= " . $to;
+        $base_url .= "&amp;to=" . $to;
+    }
 }
 
-if( $nv_Request->isset_request( "from_signer", "get" ) )
-{
-	$from_signer = $nv_Request->get_int( 'from_signer', 'get', 0 );
+if ($nv_Request->isset_request("from_signer", "get")) {
+    $from_signer = $nv_Request->get_int('from_signer', 'get', 0);
 
-	if( $from_signer != 0 )
-
-	{
-		$sql .= " AND from_signer=" . $from_signer;
-		$base_url .= "&amp;from_signer=" . $from_signer;
-	}
+    if ($from_signer != 0) {
+        $sql .= " AND from_signer=" . $from_signer;
+        $base_url .= "&amp;from_signer=" . $from_signer;
+    }
 }
 
-if( $nv_Request->isset_request( "title", "get" ) )
-{
-	$title = $nv_Request->get_string( 'title', 'get', '' );
-	if( $title != '' )
-	{
-		$page_title = sprintf( $lang_module['print_title'], '...' . $title . '...' );
-	}
-	$sql .= " AND title LIKE '%" . $title . "%' ";
-	$base_url .= "&amp;title=" . $title;
+if ($nv_Request->isset_request("title", "get")) {
+    $title = $nv_Request->get_title('title', 'get', '');
+    if ($title != '') {
+        $page_title = sprintf($lang_module['print_title'], '...' . $title . '...');
+    }
+    $sql .= " AND title LIKE '%" . $db->dblikeescape($title) . "%' ";
+    $base_url .= "&amp;title=" . urlencode($title);
 }
-if( $nv_Request->isset_request( "code", "get" ) )
-{
-	$code = $nv_Request->get_string( 'code', 'get', '' );
-	if( $code != '' )
-	{
-		$page_title = sprintf( $lang_module['print_code'], '...' . $code . '...' );
-	}
-	$sql .= " AND code LIKE '%" . $code . "%' ";
-	$base_url .= "&amp;code=" . $code;
+if ($nv_Request->isset_request("code", "get")) {
+    $code = $nv_Request->get_title('code', 'get', '');
+    if ($code != '') {
+        $page_title = sprintf($lang_module['print_code'], '...' . $code . '...');
+    }
+    $sql .= " AND code LIKE '%" . $db->dblikeescape($code) . "%' ";
+    $base_url .= "&amp;code=" . urlencode($code);
 }
 
-if( $nv_Request->isset_request( "content", "get" ) )
-{
-	$content = $nv_Request->get_string( 'content', 'get', '' );
-	$sql .= " AND content LIKE '%" . $content . "%' ";
-	$base_url .= "&amp;content=" . $content;
+if ($nv_Request->isset_request("content", "get")) {
+    $content = $nv_Request->get_title('content', 'get', '');
+    $sql .= " AND content LIKE '%" . $db->dblikeescape($content) . "%' ";
+    $base_url .= "&amp;content=" . urlencode($content);
 }
 
 $sql1 = "SELECT COUNT(*) " . $sql;
 
-$result1 = $db->query( $sql1 );
-$all_page = $result1->fetchColumn( );
+$result1 = $db->query($sql1);
+$all_page = $result1->fetchColumn();
 
-if( !$all_page )
-{
-	$error = 'Không tìm thấy văn bản bạn tìm kiếm';
+if (!$all_page) {
+    $error = 'Không tìm thấy văn bản bạn tìm kiếm';
 }
 
 $sql .= " ORDER BY from_time DESC";
 
-$page = $nv_Request->get_int( 'page', 'get', 0 );
+$page = $nv_Request->get_int('page', 'get', 0);
 $per_page = 30;
 $sql2 = "SELECT * " . $sql . " LIMIT " . $page . ", " . $per_page;
 
-$query2 = $db->query( $sql2 );
+$query2 = $db->query($sql2);
 
-$array = array( );
+$array = array();
 $i = 0;
 
-while( $row = $query2->fetch( ) )
-{
-	$i = $i + 1;
+while ($row = $query2->fetch()) {
+    $i = $i + 1;
 
-	if( $listtypes[$row['type']]['status'] == 1 && nv_user_in_groups( $row['groups_view'] ) )
-	{
-		if( nv_date( 'd.m.Y', $row['from_time'] ) == nv_date( 'd.m.Y', NV_CURRENTTIME ) )
-		{
-			$row['code'] = $row['code'] . "<img src=\"" . NV_BASE_SITEURL . "themes/" . $module_info['template'] . "/images/" . $module_file . "/new.gif\">";
-		}
-		if( strlen( $row['content'] ) > 100 )
-		{
-			$row['content'] = nv_clean60( $row['content'], 100 );
-		}
-		$row['to_org'] = '- ' . $row['to_org'];
-		if( strpos( $row['to_org'], ',' ) )
-		{
-			$row['to_org'] = str_replace( ',', '<br />- ', $row['to_org'] );
+    if ($listtypes[$row['type']]['status'] == 1 && nv_user_in_groups($row['groups_view'])) {
+        if (nv_date('d.m.Y', $row['from_time']) == nv_date('d.m.Y', NV_CURRENTTIME)) {
+            $row['code'] = $row['code'] . "<img src=\"" . NV_BASE_SITEURL . "themes/" . $module_info['template'] . "/images/" . $module_file . "/new.gif\">";
+        }
+        if (strlen($row['content']) > 100) {
+            $row['content'] = nv_clean60($row['content'], 100);
+        }
+        $row['to_org'] = '- ' . $row['to_org'];
+        if (strpos($row['to_org'], ',')) {
+            $row['to_org'] = str_replace(',', '<br />- ', $row['to_org']);
 
-		}
+        }
 
-		$array[$row['id']] = array( //
-			'id' => ( int )$row['id'], //
-			'stt' => $i, //
-			'title' => $row['title'], //
-			'code' => $row['code'], //
-			'from_org' => $row['from_org'], //
-			'to_org' => $row['to_org'], //
-			'cat' => $listcats[$row['catid']]['title'], //
-			'type' => $listtypes[$row['type']]['title'], //
-			'file' => $row['file'], //
-			'content' => $row['content'], //
-			'link_type' => NV_BASE_SITEURL . 'index.php?' . NV_LANG_VARIABLE . '=' . NV_LANG_DATA . '&amp;' . NV_NAME_VARIABLE . '=' . $module_name . "&amp;type=" . $row['type'], //
-			'link_cat' => NV_BASE_SITEURL . 'index.php?' . NV_LANG_VARIABLE . '=' . NV_LANG_DATA . '&amp;' . NV_NAME_VARIABLE . '=' . $module_name . "&amp;type=" . $row['type'] . "&catid=" . $row['catid'], //
-			'from_times' => $row['from_time'], //
-			'from_time' => nv_date( 'd.m.Y', $row['from_time'] ), //
-			'status' => $arr_status[$row['status']]['name'], //
-			'link_code' => NV_BASE_SITEURL . 'index.php?' . NV_LANG_VARIABLE . '=' . NV_LANG_DATA . '&amp;' . NV_NAME_VARIABLE . '=' . $module_name . "&amp;op=detail/" . $row['alias'] //
-		);
-	}
+        $array[$row['id']] = array( //
+            'id' => (int)$row['id'], //
+            'stt' => $i, //
+            'title' => $row['title'], //
+            'code' => $row['code'], //
+            'from_org' => $row['from_org'], //
+            'to_org' => $row['to_org'], //
+            'cat' => $listcats[$row['catid']]['title'], //
+            'type' => $listtypes[$row['type']]['title'], //
+            'file' => $row['file'], //
+            'content' => $row['content'], //
+            'link_type' => NV_BASE_SITEURL . 'index.php?' . NV_LANG_VARIABLE . '=' . NV_LANG_DATA . '&amp;' . NV_NAME_VARIABLE . '=' . $module_name . "&amp;type=" . $row['type'], //
+            'link_cat' => NV_BASE_SITEURL . 'index.php?' . NV_LANG_VARIABLE . '=' . NV_LANG_DATA . '&amp;' . NV_NAME_VARIABLE . '=' . $module_name . "&amp;type=" . $row['type'] . "&catid=" . $row['catid'], //
+            'from_times' => $row['from_time'], //
+            'from_time' => nv_date('d.m.Y', $row['from_time']), //
+            'status' => $arr_status[$row['status']]['name'], //
+            'link_code' => NV_BASE_SITEURL . 'index.php?' . NV_LANG_VARIABLE . '=' . NV_LANG_DATA . '&amp;' . NV_NAME_VARIABLE . '=' . $module_name . "&amp;op=detail/" . $row['alias'] //
+        );
+    }
 }
 
-if( empty( $array ) )
-{
-	$error = $lang_module['error_rows'];
+if (empty($array)) {
+    $error = $lang_module['error_rows'];
 }
 
-$contents = nv_theme_congvan_main( $error, $array, $page_title, $base_url, $all_page, $per_page, $page, $type, $se, $to, $from, $from_signer, $content, $code );
+$contents = nv_theme_congvan_main($error, $array, $page_title, $base_url, $all_page, $per_page, $page, $type, $se, $to, $from, $from_signer, $content, $code);
 
 include NV_ROOTDIR . '/includes/header.php';
-echo nv_site_theme( $contents );
+echo nv_site_theme($contents);
 include NV_ROOTDIR . '/includes/footer.php';
