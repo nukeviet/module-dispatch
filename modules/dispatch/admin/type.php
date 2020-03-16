60fc375e686e9c75d83abc14b64897543eabd275
Phan Tan Dung <phantandung92@gmail.com>
2020-03-16 20:03:23 +0700

    Sửa lỗi xóa loại công văn (fix #6)

--- modules/dispatch/admin/type.php
+++ modules/dispatch/admin/type.php
@@ -8,49 +8,51 @@
  * @Createdate Tue, 19 Jul 2011 09:07:26 GMT
  */
 
-if (!defined('NV_IS_FILE_ADMIN')) die('Stop!!!');
+if (!defined('NV_IS_FILE_ADMIN')) {
+    die('Stop!!!');
+}
 
 /**
  * nv_FixWeighttype()
- * 
+ *
  * @param integer $parentid
  * @return
  */
 function nv_FixWeighttype($parentid = 0)
 {
     global $db, $module_data;
-    
-    $sql = "SELECT `id` FROM `" . NV_PREFIXLANG . "_" . $module_data . "_type` WHERE `parentid`=" . $parentid . " ORDER BY `weight` ASC";
+
+    $sql = "SELECT id FROM " . NV_PREFIXLANG . "_" . $module_data . "_type WHERE parentid=" . $parentid . " ORDER BY weight ASC";
     $result = $db->query($sql);
     $weight = 0;
     while ($row = $result->fetch()) {
         $weight++;
-        $db->query("UPDATE `" . NV_PREFIXLANG . "_" . $module_data . "_type` SET `weight`=" . $weight . " WHERE `id`=" . $row['id']);
+        $db->query("UPDATE " . NV_PREFIXLANG . "_" . $module_data . "_type SET weight=" . $weight . " WHERE id=" . $row['id']);
     }
 }
 
 /**
  * nv_del_type()
- * 
+ *
  * @param mixed $typeid
  * @return
  */
 function nv_del_type($typeid)
 {
-    global $db, $module_data;
-    
-    $sql = "DELETE FROM `" . NV_PREFIXLANG . "_" . $module_data . "_document` WHERE `typeid`=" . $typeid;
+    global $db, $module_data, $admin_info;
+
+    $sql = "DELETE FROM " . NV_PREFIXLANG . "_" . $module_data . "_document WHERE type=" . $typeid;
     $db->query($sql);
-    
-    $sql = "SELECT `id` FROM `" . NV_PREFIXLANG . "_" . $module_data . "_type` WHERE `parentid`=" . $typeid;
+
+    $sql = "SELECT id FROM " . NV_PREFIXLANG . "_" . $module_data . "_type WHERE parentid=" . $typeid;
     $result = $db->query($sql);
     while (list ($id) = $result->fetch(3)) {
         nv_del_type($id);
     }
-    
-    $sql = "DELETE FROM `" . NV_PREFIXLANG . "_" . $module_data . "_type` WHERE `id`=" . $typeid;
+
+    $sql = "DELETE FROM " . NV_PREFIXLANG . "_" . $module_data . "_type WHERE id=" . $typeid;
     $db->query($sql);
-    nv_insert_logs(NV_LANG_DATA, $module_data, "delete dispatch", '', $admin_info['userid'], '');
+    nv_insert_logs(NV_LANG_DATA, $module_data, "delete dispatch", '', $admin_info['admin_id']);
 }
 
 $array = array();
@@ -58,62 +60,62 @@ $error = "";
 
 //them chu de
 if ($nv_Request->isset_request('add', 'get')) {
-    
+
     $page_title = $lang_module['type_add'];
     $is_error = false;
     if ($nv_Request->isset_request('submit', 'post')) {
         $array['parentid'] = $nv_Request->get_int('parentid', 'post', 0);
         $array['title'] = $nv_Request->get_title('title', 'post', '', 1);
-        
+
         $array['alias'] = change_alias($array['title']);
-        
+
         if (empty($array['title'])) {
             $error = $lang_module['type_err_notitle'];
             $is_error = true;
         } else {
             if (!empty($array['parentid'])) {
-                $sql = "SELECT COUNT(*) AS count FROM `" . NV_PREFIXLANG . "_" . $module_data . "_type` WHERE `id`=" . $array['parentid'];
+                $sql = "SELECT COUNT(*) AS count FROM " . NV_PREFIXLANG . "_" . $module_data . "_type WHERE id=" . $array['parentid'];
                 $result = $db->query($sql);
                 $count = $result->fetchColumn();
-                
+
                 if (!$count) {
                     $error = $lang_module['type_err_noexist'];
                     $is_error = true;
                 }
             }
-            
+
             if (!$is_error) {
-                $sql = "SELECT COUNT(*) AS count FROM `" . NV_PREFIXLANG . "_" . $module_data . "_type` WHERE `alias`=" . $db->quote($array['alias']);
+                $sql = "SELECT COUNT(*) AS count FROM " . NV_PREFIXLANG . "_" . $module_data . "_type WHERE alias=" . $db->quote($array['alias']);
                 $result = $db->query($sql);
                 $count = $result->fetchColumn();
-                
+
                 if ($count) {
                     $error = $lang_module['type_err_exist'];
                     $is_error = true;
                 }
             }
         }
-        
+
         if (!$is_error) {
-            $sql = "SELECT MAX(weight) AS new_weight FROM `" . NV_PREFIXLANG . "_" . $module_data . "_type` WHERE `parentid`=" . $array['parentid'];
+            $sql = "SELECT MAX(weight) AS new_weight FROM " . NV_PREFIXLANG . "_" . $module_data . "_type WHERE parentid=" . $array['parentid'];
             $result = $db->query($sql);
             $new_weight = $result->fetchColumn();
             $new_weight = (int) $new_weight;
             $new_weight++;
-            
-            $sql = "INSERT INTO `" . NV_PREFIXLANG . "_" . $module_data . "_type` VALUES (
-            NULL, 
-            " . $array['parentid'] . ", 
+
+            $sql = "INSERT INTO " . NV_PREFIXLANG . "_" . $module_data . "_type VALUES (
+            NULL,
+            " . $array['parentid'] . ",
             '',
             " . $db->quote($array['title']) . ",
-            " . $new_weight . ", 
+            " . $new_weight . ",
             1)";
-            
+
             $typeid = $db->insert_id($sql);
-            $query = "UPDATE `" . NV_PREFIXLANG . "_" . $module_data . "_type` SET 
-                		`alias`=" . $db->quote($array['alias'] . "-" . $typeid) . " WHERE `id`=" . $typeid;
+            $query = "UPDATE " . NV_PREFIXLANG . "_" . $module_data . "_type SET
+                        alias=" . $db->quote($array['alias'] . "-" . $typeid) . " WHERE id=" . $typeid;
             $db->query($query);
-            
+
             if (!$typeid) {
                 $error = $lang_module['type_err_save'];
                 $is_error = true;
@@ -129,7 +131,7 @@ if ($nv_Request->isset_request('add', 'get')) {
         $array['alias'] = "";
         $array['description'] = "";
     }
-    
+
     $listtypes = array(
         array(
             'id' => 0,
@@ -142,73 +144,73 @@ if ($nv_Request->isset_request('add', 'get')) {
     $xtpl->assign('FORM_ACTION', NV_BASE_ADMINURL . "index.php?" . NV_LANG_VARIABLE . "=" . NV_LANG_DATA . "&amp;" . NV_NAME_VARIABLE . "=" . $module_name . "&amp;" . NV_OP_VARIABLE . "=" . $op . "&amp;add=1");
     $xtpl->assign('LANG', $lang_module);
     $xtpl->assign('DATA', $array);
-    
+
     if (!empty($error)) {
         $xtpl->assign('ERROR', $error);
         $xtpl->parse('main.error');
     }
-    
+
     foreach ($listtypes as $type) {
         $xtpl->assign('LISTTYPES', $type);
         $xtpl->parse('main.parentid');
     }
-    
+
     $xtpl->parse('main');
     $contents = $xtpl->text('main');
-    
+
     include NV_ROOTDIR . '/includes/header.php';
     echo nv_admin_theme($contents);
     include NV_ROOTDIR . '/includes/footer.php';
-    
+
     exit();
 }
 
 //Sua chu de
 if ($nv_Request->isset_request('edit', 'get')) {
     $page_title = $lang_module['type_edit'];
-    
+
     $typeid = $nv_Request->get_int('typeid', 'get', 0);
-    
+
     if (empty($typeid)) {
         nv_redirect_location(NV_BASE_ADMINURL . "index.php?" . NV_LANG_VARIABLE . "=" . NV_LANG_DATA . "&" . NV_NAME_VARIABLE . "=" . $module_name . "&" . NV_OP_VARIABLE . "=type");
     }
-    
-    $sql = "SELECT * FROM `" . NV_PREFIXLANG . "_" . $module_data . "_type` WHERE `id`=" . $typeid;
+
+    $sql = "SELECT * FROM " . NV_PREFIXLANG . "_" . $module_data . "_type WHERE id=" . $typeid;
     $result = $db->query($sql);
     $numtype = $result->rowCount();
-    
+
     if ($numtype != 1) {
         nv_redirect_location(NV_BASE_ADMINURL . "index.php?" . NV_LANG_VARIABLE . "=" . NV_LANG_DATA . "&" . NV_NAME_VARIABLE . "=" . $module_name . "&" . NV_OP_VARIABLE . "=type");
     }
-    
+
     $row = $result->fetch();
-    
+
     $is_error = false;
-    
+
     if ($nv_Request->isset_request('submit', 'post')) {
         $array['parentid'] = $nv_Request->get_int('parentid', 'post', 0);
         $array['title'] = $nv_Request->get_title('title', 'post', '', 1);
         $array['introduction'] = $nv_Request->get_title('introduction', 'post', '');
-        
+
         $array['alias'] = change_alias($array['title']);
-        
+
         if (empty($array['title'])) {
             $error = $lang_module['type_err_notitle'];
             $is_error = true;
         } else {
             if (!empty($array['parentid'])) {
-                $sql = "SELECT COUNT(*) AS count FROM `" . NV_PREFIXLANG . "_" . $module_data . "_type` WHERE `id`=" . $array['parentid'];
+                $sql = "SELECT COUNT(*) AS count FROM " . NV_PREFIXLANG . "_" . $module_data . "_type WHERE id=" . $array['parentid'];
                 $result = $db->query($sql);
                 $count = $result->fetchColumn();
-                
+
                 if (!$count) {
                     $error = $lang_module['type_err_noexist'];
                     $is_error = true;
                 }
             }
-            
+
             if (!$is_error) {
-                $sql = "SELECT COUNT(*) AS count FROM `" . NV_PREFIXLANG . "_" . $module_data . "_type` WHERE `id`!=" . $typeid . " AND `alias`=" . $db->quote($array['alias']);
+                $sql = "SELECT COUNT(*) AS count FROM " . NV_PREFIXLANG . "_" . $module_data . "_type WHERE id!=" . $typeid . " AND alias=" . $db->quote($array['alias']);
                 $result = $db->query($sql);
                 $count = $result->fetchColumn();
                 if ($count) {
@@ -217,10 +219,10 @@ if ($nv_Request->isset_request('edit', 'get')) {
                 }
             }
         }
-        
+
         if (!$is_error) {
             if ($array['parentid'] != $row['parentid']) {
-                $sql = "SELECT MAX(weight) AS new_weight FROM `" . NV_PREFIXLANG . "_" . $module_data . "_type` WHERE `parentid`=" . $array['parentid'];
+                $sql = "SELECT MAX(weight) AS new_weight FROM " . NV_PREFIXLANG . "_" . $module_data . "_type WHERE parentid=" . $array['parentid'];
                 $result = $db->query($sql);
                 $new_weight = $result->fetchColumn();
                 $new_weight = (int) $new_weight;
@@ -228,19 +230,19 @@ if ($nv_Request->isset_request('edit', 'get')) {
             } else {
                 $new_weight = $row['weight'];
             }
-            
-            $sql = "UPDATE `" . NV_PREFIXLANG . "_" . $module_data . "_type` SET 
-            `parentid`=" . $array['parentid'] . ", 
-            `title`=" . $db->quote($array['title']) . ", 
-            `alias`='',            
-            `weight`=" . $new_weight . " 
-            WHERE `id`=" . $typeid;
+
+            $sql = "UPDATE " . NV_PREFIXLANG . "_" . $module_data . "_type SET
+            parentid=" . $array['parentid'] . ",
+            title=" . $db->quote($array['title']) . ",
+            alias='',
+            weight=" . $new_weight . "
+            WHERE id=" . $typeid;
             $result = $db->query($sql);
-            
-            $query = "UPDATE `" . NV_PREFIXLANG . "_" . $module_data . "_type` SET 
-                		`alias`=" . $db->quote($array['alias'] . "-" . $typeid) . " WHERE `id`=" . $typeid;
+
+            $query = "UPDATE " . NV_PREFIXLANG . "_" . $module_data . "_type SET
+                        alias=" . $db->quote($array['alias'] . "-" . $typeid) . " WHERE id=" . $typeid;
             $db->query($query);
-            
+
             if (!$result) {
                 $error = $lang_module['type_err_update'];
                 $is_error = true;
@@ -248,7 +250,7 @@ if ($nv_Request->isset_request('edit', 'get')) {
                 if ($array['parentid'] != $row['parentid']) {
                     nv_FixWeighttype($row['parentid']);
                 }
-                
+
                 $nv_Cache->delMod($module_name);
                 nv_insert_logs(NV_LANG_DATA, $module_name, $lang_module['type_edit'], $array['title'], $admin_info['userid']);
                 nv_redirect_location(NV_BASE_ADMINURL . "index.php?" . NV_LANG_VARIABLE . "=" . NV_LANG_DATA . "&" . NV_NAME_VARIABLE . "=" . $module_name . "&" . NV_OP_VARIABLE . "=type");
@@ -259,7 +261,7 @@ if ($nv_Request->isset_request('edit', 'get')) {
         $array['title'] = $row['title'];
         $array['alias'] = $row['alias'];
     }
-    
+
     $listtypes = array(
         array(
             'id' => 0,
@@ -268,109 +270,109 @@ if ($nv_Request->isset_request('edit', 'get')) {
         )
     );
     $listtypes = $listtypes + nv_listtypes($array['parentid']);
-    
+
     $xtpl = new XTemplate("type_add.tpl", NV_ROOTDIR . "/themes/" . $global_config['module_theme'] . "/modules/" . $module_file);
     $xtpl->assign('FORM_ACTION', NV_BASE_ADMINURL . "index.php?" . NV_LANG_VARIABLE . "=" . NV_LANG_DATA . "&amp;" . NV_NAME_VARIABLE . "=" . $module_name . "&amp;" . NV_OP_VARIABLE . "=" . $op . "&amp;edit=1&amp;typeid=" . $typeid);
     $xtpl->assign('LANG', $lang_module);
     $xtpl->assign('DATA', $array);
-    
+
     if (!empty($error)) {
         $xtpl->assign('ERROR', $error);
         $xtpl->parse('main.error');
     }
-    
+
     foreach ($listtypes as $type) {
         $xtpl->assign('LISTTYPES', $type);
         $xtpl->parse('main.parentid');
     }
-    
+
     $xtpl->parse('main');
     $contents = $xtpl->text('main');
-    
+
     include NV_ROOTDIR . '/includes/header.php';
     echo nv_admin_theme($contents);
     include NV_ROOTDIR . '/includes/footer.php';
-    
+
     exit();
 }
 
 //Xoa chu de
 if ($nv_Request->isset_request('del', 'post')) {
     if (!defined('NV_IS_AJAX')) die('Wrong URL');
-    
+
     $typeid = $nv_Request->get_int('typeid', 'post', 0);
-    
+
     if (empty($typeid)) {
         die('NO');
     }
-    
-    $sql = "SELECT COUNT(*) AS count, `parentid` FROM `" . NV_PREFIXLANG . "_" . $module_data . "_type` WHERE `id`=" . $typeid;
+
+    $sql = "SELECT COUNT(*) AS count, parentid FROM " . NV_PREFIXLANG . "_" . $module_data . "_type WHERE id=" . $typeid;
     $result = $db->query($sql);
     list ($count, $parentid) = $result->fetch(3);
-    
+
     if ($count != 1) {
         die('NO');
     }
-    
+
     nv_del_type($typeid);
     nv_FixWeighttype($parentid);
     $nv_Cache->delMod($module_name);
-    
+
     nv_htmlOutput('OK');
 }
 //Chinh thu tu chu de
 if ($nv_Request->isset_request('changeweight', 'post')) {
     if (!defined('NV_IS_AJAX')) die('Wrong URL');
-    
+
     $typeid = $nv_Request->get_int('typeid', 'post', 0);
     $new = $nv_Request->get_int('new', 'post', 0);
-    
+
     if (empty($typeid)) die('NO');
-    
-    $query = "SELECT `parentid` FROM `" . NV_PREFIXLANG . "_" . $module_data . "_type` WHERE `id`=" . $typeid;
+
+    $query = "SELECT parentid FROM " . NV_PREFIXLANG . "_" . $module_data . "_type WHERE id=" . $typeid;
     $result = $db->query($query);
     $numrows = $result->rowCount();
     if ($numrows != 1) die('NO');
     $parentid = $result->fetchColumn();
-    
-    $query = "SELECT `id` FROM `" . NV_PREFIXLANG . "_" . $module_data . "_type` WHERE `id`!=" . $typeid . " AND `parentid`=" . $parentid . " ORDER BY `weight` ASC";
+
+    $query = "SELECT id FROM " . NV_PREFIXLANG . "_" . $module_data . "_type WHERE id!=" . $typeid . " AND parentid=" . $parentid . " ORDER BY weight ASC";
     $result = $db->query($query);
     $weight = 0;
     while ($row = $result->fetch()) {
         $weight++;
         if ($weight == $new) $weight++;
-        $sql = "UPDATE `" . NV_PREFIXLANG . "_" . $module_data . "_type` SET `weight`=" . $weight . " WHERE `id`=" . $row['id'];
+        $sql = "UPDATE " . NV_PREFIXLANG . "_" . $module_data . "_type SET weight=" . $weight . " WHERE id=" . $row['id'];
         $db->query($sql);
     }
-    $sql = "UPDATE `" . NV_PREFIXLANG . "_" . $module_data . "_type` SET `weight`=" . $new . " WHERE `id`=" . $typeid;
+    $sql = "UPDATE " . NV_PREFIXLANG . "_" . $module_data . "_type SET weight=" . $new . " WHERE id=" . $typeid;
     $db->query($sql);
-    
+
     $nv_Cache->delMod($module_name);
-    
+
     nv_htmlOutput('OK');
 }
 
 //Kich hoat - dinh chi
 if ($nv_Request->isset_request('changestatus', 'post')) {
     if (!defined('NV_IS_AJAX')) die('Wrong URL');
-    
+
     $typeid = $nv_Request->get_int('typeid', 'post', 0);
-    
+
     if (empty($typeid)) die('NO');
-    
-    $query = "SELECT `status` FROM `" . NV_PREFIXLANG . "_" . $module_data . "_type` WHERE `id`=" . $typeid;
+
+    $query = "SELECT status FROM " . NV_PREFIXLANG . "_" . $module_data . "_type WHERE id=" . $typeid;
     $result = $db->query($query);
     $numrows = $result->rowCount();
     if ($numrows != 1) die('NO');
-    
+
     $status = $result->fetchColumn();
     $status = $status ? 0 : 1;
-    
-    $sql = "UPDATE `" . NV_PREFIXLANG . "_" . $module_data . "_type` SET `status`=" . $status . " WHERE `id`=" . $typeid;
+
+    $sql = "UPDATE " . NV_PREFIXLANG . "_" . $module_data . "_type SET status=" . $status . " WHERE id=" . $typeid;
     $db->query($sql);
-    
+
     $nv_Cache->delMod($module_name);
-    
+
     nv_htmlOutput('OK');
 }
 
@@ -379,24 +381,24 @@ $page_title = $lang_module['type_list'];
 
 $pid = $nv_Request->get_int('pid', 'get', 0);
 
-$sql = "SELECT * FROM `" . NV_PREFIXLANG . "_" . $module_data . "_type` WHERE `parentid`=" . $pid . " ORDER BY `weight` ASC";
+$sql = "SELECT * FROM " . NV_PREFIXLANG . "_" . $module_data . "_type WHERE parentid=" . $pid . " ORDER BY weight ASC";
 $result = $db->query($sql);
 $num = $result->rowCount();
 
 if (!$num) {
     if ($pid) {
         Header("Location: " . NV_BASE_ADMINURL . "index.php?" . NV_LANG_VARIABLE . "=" . NV_LANG_DATA . "&" . NV_NAME_VARIABLE . "=" . $module_name . "&" . NV_OP_VARIABLE . "=type");
-    
+
     } else {
-        
+
         Header("Location: " . NV_BASE_ADMINURL . "index.php?" . NV_LANG_VARIABLE . "=" . NV_LANG_DATA . "&" . NV_NAME_VARIABLE . "=" . $module_name . "&" . NV_OP_VARIABLE . "=type&add=1");
-    
+
     }
     exit();
 }
 
 if ($pid) {
-    $sql2 = "SELECT `title`,`parentid` FROM `" . NV_PREFIXLANG . "_" . $module_data . "_type` WHERE `id`=" . $pid;
+    $sql2 = "SELECT title,parentid FROM " . NV_PREFIXLANG . "_" . $module_data . "_type WHERE id=" . $pid;
     $result2 = $db->query($sql2);
     list ($parentid, $parentid2) = $result2->fetch(3);
     $caption = sprintf($lang_module['table_type2'], $parentid);
@@ -410,22 +412,22 @@ $list = array();
 $a = 0;
 
 while ($row = $result->fetch()) {
-    $numsub = $db->query("SELECT `id` FROM `" . NV_PREFIXLANG . "_" . $module_data . "_type` WHERE parentid=" . $row['id'])->rowCount();
+    $numsub = $db->query("SELECT id FROM " . NV_PREFIXLANG . "_" . $module_data . "_type WHERE parentid=" . $row['id'])->rowCount();
     if ($numsub) {
         $numsub = " (<a href=\"" . NV_BASE_ADMINURL . "index.php?" . NV_LANG_VARIABLE . "=" . NV_LANG_DATA . "&amp;" . NV_NAME_VARIABLE . "=" . $module_name . "&amp;" . NV_OP_VARIABLE . "=type&amp;pid=" . $row['id'] . "\">" . $numsub . " " . $lang_module['type_sub'] . "</a>)";
     } else {
         $numsub = "";
     }
-    
+
     $weight = array();
     for ($i = 1; $i <= $num; $i++) {
         $weight[$i]['title'] = $i;
         $weight[$i]['pos'] = $i;
         $weight[$i]['selected'] = ($i == $row['weight']) ? " selected=\"selected\"" : "";
     }
-    
+
     $class = ($a % 2) ? " class=\"second\"" : "";
-    
+
     $list[$row['id']] = array( //
         'id' => (int) $row['id'], //
         'title' => $row['title'], //
@@ -437,7 +439,7 @@ while ($row = $result->fetch()) {
         'class' => $class
     ); //
 
-    
+
     $a++;
 }
 
@@ -454,12 +456,12 @@ $xtpl->assign('OP', $op);
 
 foreach ($list as $row) {
     $xtpl->assign('ROW', $row);
-    
+
     foreach ($row['weight'] as $weight) {
         $xtpl->assign('WEIGHT', $weight);
         $xtpl->parse('main.row.weight');
     }
-    
+
     $xtpl->assign('EDIT_URL', NV_BASE_ADMINURL . "index.php?" . NV_LANG_VARIABLE . "=" . NV_LANG_DATA . "&amp;" . NV_NAME_VARIABLE . "=" . $module_name . "&amp;" . NV_OP_VARIABLE . "=type&amp;edit=1&amp;typeid=" . $row['id']);
     $xtpl->parse('main.row');
 }
--- themes/admin_default/modules/dispatch/type_list.tpl
+++ themes/admin_default/modules/dispatch/type_list.tpl
@@ -2,63 +2,58 @@
 <script type="text/javascript">
     var type_del_cofirm = "{LANG.type_del}";
 </script>
-
-<div id="users">
-    <table class="table table-striped table-bordered table-hover">
-        <caption>{TABLE_CAPTION}</caption>
-        <colgroup>
-            <col class="w100" />
-            <col span="2" />
-            <col span="2" class="w150" />
-        </colgroup>
-        <thead>
-            <tr>
-                <th>
-                    {LANG.type_sort}
-                </th>
-                <th>
-                    {LANG.type_name}
-                </th>
-                <th>
-                    {LANG.type_parent}
-                </th>
-                <th  class="text-center">
-                    {LANG.type_active}
-                </th>
-                <th class="text-center">
-                    {LANG.feature}
-                </th>
-            </tr>
-        </thead>
-        <tbody>
+<table class="table table-striped table-bordered table-hover">
+    <caption>{TABLE_CAPTION}</caption>
+    <colgroup>
+        <col class="w100" />
+        <col span="2" />
+        <col span="2" class="w200" />
+    </colgroup>
+    <thead>
+        <tr>
+            <th>
+                {LANG.type_sort}
+            </th>
+            <th>
+                {LANG.type_name}
+            </th>
+            <th>
+                {LANG.type_parent}
+            </th>
+            <th  class="text-center">
+                {LANG.type_active}
+            </th>
+            <th class="text-center">
+                {LANG.feature}
+            </th>
+        </tr>
+    </thead>
+    <tbody>
         <!-- BEGIN: row -->
-            <tr>
-                <td>
-                    <select class="form-control" name="weight" id="weight{ROW.id}" onchange="nv_chang_type_weight({ROW.id});">
-                        <!-- BEGIN: weight -->
-                        <option value="{WEIGHT.pos}"{WEIGHT.selected}>{WEIGHT.pos}</option>
-                        <!-- END: weight -->
-                    </select>
-                </td>
-                <td>
-                    <strong><a href="{ROW.titlelink}">{ROW.title}</a></strong>{ROW.numsub}
-                </td>
-                <td>
-                    {ROW.parentid}
-                </td>
-                <td  class="text-center">
-                    <input type="checkbox" name="active" id="change_status{ROW.id}" value="1"{ROW.status} onclick="nv_chang_type_status({ROW.id});" />
-                </td>
-                <td  class="text-center">
-                    <em class="fa fa-edit fa-lg">&nbsp;</em><a href="{EDIT_URL}">{GLANG.edit}</a> -
-                    <em class="fa fa-trash-o fa-lg">&nbsp;</em><a href="javascript:void(0);" onclick="nv_type_del({ROW.id});">{GLANG.delete}</a>
-                </td>
-            </tr>
+        <tr>
+            <td>
+                <select class="form-control input-sm" name="weight" id="weight{ROW.id}" onchange="nv_chang_type_weight({ROW.id});">
+                    <!-- BEGIN: weight -->
+                    <option value="{WEIGHT.pos}"{WEIGHT.selected}>{WEIGHT.pos}</option>
+                    <!-- END: weight -->
+                </select>
+            </td>
+            <td>
+                <strong><a href="{ROW.titlelink}">{ROW.title}</a></strong>{ROW.numsub}
+            </td>
+            <td>
+                {ROW.parentid}
+            </td>
+            <td  class="text-center">
+                <input type="checkbox" name="active" id="change_status{ROW.id}" value="1"{ROW.status} onclick="nv_chang_type_status({ROW.id});" />
+            </td>
+            <td  class="text-center">
+                <a href="{EDIT_URL}" class="btn btn-default btn-sm"><i class="fa fa-edit"></i> {GLANG.edit}</a>
+                <a href="javascript:void(0);" onclick="nv_type_del({ROW.id});" class="btn btn-danger btn-sm"><i class="fa fa-trash"></i> {GLANG.delete}</a>
+            </td>
+        </tr>
         <!-- END: row -->
-        <tbody>
-    </table>
-</div>
-
+    <tbody>
+</table>
 <a class="btn btn-primary" href="{ADD_NEW_TYPE}">{LANG.type_add}</a>
-
 <!-- END: main -->
